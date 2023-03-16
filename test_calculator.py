c1ae26721d3efd67d1acd10dddd2a5cf51ead0b8
Niyamath <niyamath1998@gmail.com>
2023-03-16 18:51:46 +0300

    added multiplication and division functions

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+    
+    def test_multiplication(self):
+        assert 8 == calculator.multiplication(4, 2)
+
+    def test_division(self):
+        assert 2 == calculator.division(4, 2)
